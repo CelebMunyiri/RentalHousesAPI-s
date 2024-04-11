c520170acff3b487949d27f37f4339af0be78a97
David Munyiri <david@nathandigital.com>
2024-04-11 11:08:08 +0300

    added some changes on the main branch in the middlewares, model and controller

--- Models/auth.js
+++ Models/auth.js
@@ -1,8 +1,18 @@
 const mongoose=require('mongoose');
-
+const validator=require('validator');
 const userSchema=new mongoose.Schema({
     username:{type:String,required:true,unique:true},
-    email:{type:String,unique:true},
+    email:{
+      type:String,
+      unique:true,
+      required:true,
+      validate(value){
+        if(!validator.isEmail(value)){
+          throw new Error('Email is invalid');
+        }
+      }
+
+    },
     password:{type:String,required:true}
   });
 
