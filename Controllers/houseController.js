3218a37d533591a7377ca7c1dc2735d3b7c879b0
David Munyiri <davidmumyiri2019@gmail.com>
2024-09-09 21:37:30 +0300

    made changes on the of the create house api

--- Controllers/houseController.js
+++ Controllers/houseController.js
@@ -3,36 +3,13 @@ const dotenv = require('dotenv');
 const {success,error}=require('../Utils/responses')
 dotenv.config();
 
-const http = require('http');
-const socketIo = require('socket.io');
-const express = require('express');
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
 
-  // Emit event for new ticket
-  socket.on('newHouse', () => {
-    socket.broadcast.emit('newHouse');
-  });
 
-  // Emit event for status change
-  socket.on('statusChange', () => {
-    socket.broadcast.emit('statusChange');
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
-
-app.use(express.json());
 
 const createHouse=async(req,res)=>{
     try {
-        const {name,cost,description,images,location}=req.body;
+        const {name,cost,description,images,location,poster}=req.body;
         const isHouseinDB=await house.findOne({name});
 
        if(isHouseinDB){
@@ -43,7 +20,7 @@ const newHouse=await house.create({name,cost,description,images,location});
 
 await newHouse.save();
 
-io.emit('house_created', newHouse);
+
 res.status(201).json({success:true,message:"House Added succesfully"});
     } catch (error) {
         console.error("There was an error",error);
@@ -56,22 +33,22 @@ const updateHouse=async(req,res)=>{
         
         const houseId=req.params.houseId;
         const updatedHouse=req.body;
-        const theHouse=await house.findById(houseId);
+        const theHouse=await house.findByIdAndUpdate(houseId,updatedHouse,{new:true});
         
 
-        if(!theHouse){
-            return res.status(404).json({message:"House Does Not Exist"});
-        }
+        // if(!theHouse){
+        //     return res.status(404).json({message:"House Does Not Exist"});
+        // }
         
-        if(updatedHouse.name)theHouse.name=updatedHouse.name
-        if(updatedHouse.cost)theHouse.cost=updatedHouse.cost
-        if(updatedHouse.description)theHouse.description=updatedHouse.description
-        if(updatedHouse.location)theHouse.location=updatedHouse.location
-        if(updatedHouse.images)theHouse.images=updatedHouse.images
+        // if(updatedHouse.name)theHouse.name=updatedHouse.name
+        // if(updatedHouse.cost)theHouse.cost=updatedHouse.cost
+        // if(updatedHouse.description)theHouse.description=updatedHouse.description
+        // if(updatedHouse.location)theHouse.location=updatedHouse.location
+        // if(updatedHouse.images)theHouse.images=updatedHouse.images
 
-        await theHouse.save();
+       // await theHouse.save();
         
-        io.emit('house_updated', theHouse);
+       
         return res.status(200).json({message:"House details updated as success"});
     } catch (error) {
         console.error("There was an error",error);
