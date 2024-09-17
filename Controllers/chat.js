61bfb4d8f3aef9b63dbfeca08cfbfe9f7fc8a11d
David Munyiri <davidmumyiri2019@gmail.com>
2024-09-17 21:26:59 +0300

    continued on chat

--- Controllers/chat.js
+++ Controllers/chat.js
@@ -1,2 +1,20 @@
 const Chat = require('../Models/chat')
-const socket = require('socket.io')
+const socketIO = require('socket-io')
+
+const io = socketIO();
+
+const chatModule = {};
+chatModule.io = io;
+
+chatModule.io.on("connection",socket=>{
+    console.log("connected to server")
+    // socket.on("connection",=>{
+    //     console.log("connected")
+    // });
+
+    socket.on("chatMessage",data=>{
+        const {text, attachment,roomId} = data; 
+    socket.to(roomId).emit(data)   })
+})
+
+module.exports = { chatModule }
